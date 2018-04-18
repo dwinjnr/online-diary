cda1d7f025ca66c85c33561c003e2a2bc35a36c0
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-04-18 18:08:32 +0100

    styled the navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+<nav id="navbar" class="navbar navbar-expand-md navbar-light navbar-laravel">
   <div class="container">
       <a class="navbar-brand" href="{{ url('/') }}">
           {{ config('app.name', 'Laravel') }}
@@ -25,7 +25,7 @@
                           {{ Auth::user()->name }} <span class="caret"></span>
                       </a>
 
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <div id="dropdown" class="dropdown-menu" aria-labelledby="navbarDropdown">
                           <a class="dropdown-item" href="{{ route('logout') }}"
                              onclick="event.preventDefault();
                                            document.getElementById('logout-form').submit();">
